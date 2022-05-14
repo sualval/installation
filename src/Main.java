533329916f88bc0891192ec09184eca0287fba6b
Aleksey Subbotin <sualval@gmail.com>
2022-05-14 17:04:02 +0400

    code refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,45 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
         String PATH = "D:/Games/";
-        File src = new File(PATH, "src");
-        File res = new File(PATH, "res");
-        File saveGames = new File(PATH, "savegames");
-        File temp = new File(PATH, "temp");
-        File main = new File(src, "main");
-        File test = new File(src, "test");
-        File mainJava = new File(main, "Main.java");
-        File utilJava = new File(main, "Utils.java");
-        File drawables = new File(res, "drawables");
-        File vectors = new File(res, "vectors");
-        File icons = new File(res, "icons");
-        File temptxt = new File(temp, "temp.txt");
-        stringBuilder.append(temp(src.mkdir(), src.getName()));
-        stringBuilder.append(temp(res.mkdir(), res.getName()));
-        stringBuilder.append(temp(saveGames.mkdir(), saveGames.getName()));
-        stringBuilder.append(temp(temp.mkdir(), temp.getName()));
-        stringBuilder.append(temp(main.mkdir(), main.getName()));
-        stringBuilder.append(temp(test.mkdir(), test.getName()));
-        stringBuilder.append(temp(drawables.mkdir(), drawables.getName()));
-        stringBuilder.append(temp(vectors.mkdir(), vectors.getName()));
-        stringBuilder.append(temp(icons.mkdir(), icons.getName()));
-        try {
-            stringBuilder.append(temp(temptxt.createNewFile(), temptxt.getName()));
-            stringBuilder.append(temp(mainJava.createNewFile(), mainJava.getName()));
-            stringBuilder.append(temp(utilJava.createNewFile(), utilJava.getName()));
-        } catch (IOException exception) {
-            System.out.println(exception.getMessage());
+        List<File> dir = new ArrayList<>(Arrays.asList(new File(PATH, "src"), new File(PATH, "res"), new File(PATH, "savegames")
+                , new File(PATH, "temp"), new File(PATH + "src", "main"), new File(PATH + "src", "test")
+                , new File(PATH + "res", "drawables"), new File(PATH + "res", "vectors")
+                , new File(PATH + "res", "icons"), new File(PATH + "temp", "temp.txt"), new File(PATH + "src/main", "Main.java")
+                , new File(PATH + "src/main", "Utils.java")));
+
+        for (File file : dir) {
+            stringBuilder.append(temp(file));
         }
-        try (FileWriter fileWriter = new FileWriter(temptxt)) {
+        try (FileWriter fileWriter = new FileWriter(PATH + "temp/temp.txt")) {
             fileWriter.write(stringBuilder.toString());
         } catch (IOException exception) {
             exception.getMessage();
         }
     }
 
-    public static String temp(boolean result, String name) {
+    public static String temp(File file) {
+        boolean result = true;
+        String name = file.getName();
+        try {
+            if (name.contains(".")) {
+                result = file.createNewFile();
+            } else {
+                result = file.mkdirs();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
         return result ? name + " успешно создан\n" : name + " не создан\n";
     }
+
 
 }
 
